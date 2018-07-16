05f2e7552649492306b652f1c367c1f70115024a
sqaku <sqmchh@gmail.com>
2018-07-17 00:00:50 +0100

    fix progress bar moving other components with v-visible

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -6,42 +6,44 @@
         <md-field>
           <label>Enter subreddit name...</label>
           <md-input v-model="type"></md-input>
-
         </md-field>
       </div>
-      <md-button v-on:click="getSubs()" class="md-raised md-primary">
-        Search
-      </md-button>
-
+      <md-button v-on:click="getSubs()" class="md-raised md-primary">Search</md-button>
     </div>
-      <div class="md-layout">
-        <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio">Top</md-radio></div>
-        <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio1">Hot</md-radio></div>
-        <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio2">New</md-radio></div>
-        <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio3">Rising</md-radio></div>
-        <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio4">Controversial</md-radio></div>
-      </div>
-      <div class="md-layout-item" v-if="isLoading">
-        <md-progress-bar md-mode="indeterminate"></md-progress-bar>
-      </div>
-    <!-- DROPDOWNS -->
+    <div class="md-layout">
+      <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio">Top</md-radio></div>
+      <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio1">Hot</md-radio></div>
+      <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio2">New</md-radio></div>
+      <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio3">Rising</md-radio></div>
+      <div class=" md-medium-size-33 md-small-size-50 md-xsmall-size-100"><md-radio v-model="radio" value="my-radio4">Controversial</md-radio></div>
+    </div>
+    <!-- PROGRESS BAR -->
 
-    <!-- /DROPDOWNS -->
+      <div class="md-layout-item">
+        <md-progress-bar v-visible="isLoading" md-mode="query"></md-progress-bar>
+      </div>
+
     <!-- DATA TABLE -->
     <div class="md-layout-item">
-
-      <md-table v-model="submissiondata" md-card @md-selected="onSelect">
-        <md-table-toolbar>
-          <h1 class="md-title">Submissions</h1>
-        </md-table-toolbar>
+      <md-table md-card v-model="submissiondata" @md-selected="onSelect">
+        <md-table-empty-state
+          md-label="Hello! Let's get started..."
+          :md-description="'Enter a subreddit name, pick your sorting preference and press Search to load submissions.'">
+        </md-table-empty-state>
+        <!-- TABLE TITLE TOGGLE -->
+          <md-table-toolbar md-alignment-center>
+            <transition name="fade">
+            <h3 v-if="titleVisible" class="md-title">Submissions</h3>
+            </transition>
+          </md-table-toolbar>
         <md-table-toolbar slot="md-table-alternate-header" slot-scope="{ count }">
           <div class="md-toolbar-section-start">{{ getAlternateLabel(count) }}</div>
-
           <div class="md-toolbar-section-end">
             <md-button class="md-icon-button">
               <md-icon>delete</md-icon>
             </md-button>
           </div>
+
         </md-table-toolbar>
         <md-table-row slot="md-table-row" slot-scope="{ item }" md-selectable="multiple" md-auto-select>
           <md-table-cell md-label="Submission" md-sort-by="email">{{ item.title }}</md-table-cell>
@@ -55,8 +57,12 @@
 small {
 	display: block;
 }
-.md-progress-spinner {
-	margin: 24px;
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+	opacity: 0;
 }
 .md-radio {
 	display: flex;
@@ -67,7 +73,6 @@ small {
 .md-app {
 	border: 1px solid rgba(#000, 0.12);
 }
-
 .md-table + .md-table {
 	margin-top: 16px;
 }
@@ -80,6 +85,7 @@ export default {
   name: 'Home',
   data: () => ({
     isLoading: false,
+    titleVisible: false,
     radio: true,
     submissiondata: {},
     selected: {},
@@ -89,6 +95,15 @@ export default {
   methods: {
     onSelect (item) {
       this.selected = item
+    },
+    getAlternateLabel (count) {
+      let plural = ''
+
+      if (count > 1) {
+        plural = 's'
+      }
+
+      return `${count} submission${plural} selected`
     },
     getSubs () {
       this.isLoading = true
@@ -100,10 +115,12 @@ export default {
           this.submissiondata = dataobj
           console.log(dataobj)
           this.isLoading = false
+          this.titleVisible = true
         })
         .catch(error => {
           console.log(error)
           this.isLoading = false
+          this.titleVisible = true
         })
     }
   }
